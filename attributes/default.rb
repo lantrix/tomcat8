7b20b902c45fae0909c1a46e76cb5d10714b7d45
Bryant <blippert@pelicanteksystems.com>
2014-12-23 21:51:40 -0600

    it's working

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+
+default['tomcat']['tomcat_version'] = 'apache-tomcat-8.0.15'
+default['tomcat']['tomcat_url'] = 'http://mirrors.gigenet.com/apache/tomcat/tomcat-8/v8.0.15/bin/apache-tomcat-8.0.15.tar.gz'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,40 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+remote_file "/opt/#{node['tomcat']['tomcat_version']}.tar.gz" do
+  source "#{node['tomcat']['tomcat_url']}"
+end
+
+bash "extract_tomcat" do
+  user "root"
+  cwd "/opt"
+  code <<-EOH
+  tar -xvzf "#{node['tomcat']['tomcat_version']}.tar.gz"
+  cd "#{node['tomcat']['tomcat_version']}"
+  EOH
+end
+
+bash "create_the_tomcat_user" do
+  user "root"
+  code <<-EOH
+  adduser tomcat
+  EOH
+end
+
+bash "change_ownership_of_the_tomcat_directory_to_the_user_tomcat" do
+  user "root"
+  cwd "/opt"
+  code <<-EOH
+  chown -R tomcat "#{node['tomcat']['tomcat_version']}"
+  EOH
+end
+
+bash "start_tomcat" do
+  user "tomcat"
+  cwd "/opt/#{node['tomcat']['tomcat_version']}/bin"
+  code <<-EOH
+  ./catalina.sh start
+  EOH
+end
+
