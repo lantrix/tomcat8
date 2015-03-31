575a01a699485c5f6147e942aeb7801a1c16bb59
Lantrix <lantrix@pobox.com>
2015-03-31 16:07:44 +1100

    Apache 8.0.21

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 
-default['tomcat']['tomcat_version'] = 'apache-tomcat-8.0.15'
-default['tomcat']['tomcat_url'] = 'http://mirrors.gigenet.com/apache/tomcat/tomcat-8/v8.0.15/bin/apache-tomcat-8.0.15.tar.gz'
-default['tomcat']['checksum'] = '2cc244070d01193c541e526564068e6f4e9ecade22380e38e681e931f3dc3699'
+default['tomcat']['tomcat_version'] = 'apache-tomcat-8.0.21'
+default['tomcat']['tomcat_url'] = 'http://mirror.rackcentral.com.au/apache/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21.tar.gz'
+default['tomcat']['checksum'] = 'a009f855ac7ac910a51e2259bb24bd8b3fc1be7af75a3a9feb53f883dc6c6a88'
 default['tomcat']['password'] = '$1$FriiRKvW$rhegRwjAuhDdIcaXp6UC.1'
 default['tomcat']['installation_directory'] = '/opt'
 default['tomcat']['shell'] = '/bin/bash'
